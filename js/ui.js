0192383a97bf4ed2b20d3700ea4e85e240446207
Chayapol Dansikaew <arthurz@MacBook-Air-khxng-Chayapol.local>
2026-02-08 19:35:03 +0700

    🔄 Revert to original calendar modal flow - remove unified modal

--- js/ui.js
+++ js/ui.js
@@ -78,11 +78,11 @@ window.renderEquipments = function () {
         } else if (isInCart) {
             btnClass = 'bg-green-500 text-white cursor-pointer hover:bg-green-600';
             btnText = `<svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg> ในตะกร้า (${cartItem.quantity})`;
-            btnAction = `onclick="openQuantityModal('${group.name}', '${group.image_url}', '${group.type}', ${group.available})"`;
+            btnAction = `onclick="openEquipmentDetail(${group.items[0].id})"`;
         } else {
             btnClass = 'bg-brand-yellow text-black hover:bg-yellow-400 shadow-md cursor-pointer';
             btnText = '<svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg> ใส่ตะกร้า';
-            btnAction = `onclick="openQuantityModal('${group.name}', '${group.image_url}', '${group.type}', ${group.available})"`;
+            btnAction = `onclick="openEquipmentDetail(${group.items[0].id})"`;
         }
 
         return `
